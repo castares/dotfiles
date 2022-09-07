b3ca99fd72042b855138244b8262142420d4607f
César Castañón <cesar.castanon@piperlab.es>
2022-09-07 14:08:00 +0200

    [NVIM][lualine] add relative filepath, remove encoding & fileformat

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -10,15 +10,18 @@ lualine.setup({
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {},
-		always_divide_middle = true,
+		always_divide_middle = false,
 		globalstatus = true,
 	},
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
+		lualine_c = { {
+			"filename",
+			path = 1,
+		} },
+		lualine_x = {},
+		lualine_y = { "filetype" },
 		lualine_z = { "location" },
 	},
 	inactive_sections = {
