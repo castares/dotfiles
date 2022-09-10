ffe32b3bea3832f23b85758f02a0cfae5ea603e3
César C <cesar.castares@gmail.com>
2022-09-10 19:02:45 +0200

    [NVIM][colorschemes] Add directory to save colorscheme setups

---
+++ nvim/.config/nvim/lua/user/colorschemes/init.lua
@@ -0,0 +1,9 @@
+require("user.colorschemes.nightfox")
+
+local colorscheme = "nightfox"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
+end
--- nvim/.config/nvim/lua/user/colorschemes/nightfox.lua
+++ nvim/.config/nvim/lua/user/colorschemes/nightfox.lua
@@ -1,22 +1,28 @@
 -- Default options
-require("nightfox").setup({
+local status_ok, nightfox = pcall(require, "nightfox")
+if not status_ok then
+	vim.notify("plugin " .. nightfox .. " not found!")
+	return
+end
+
+nightfox.setup({
 	options = {
 		-- Compiled file's destination location
-		-- compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+		compile_path = vim.fn.stdpath("cache") .. "/nightfox",
 		compile_file_suffix = "_compiled", -- Compiled file suffix
-		transparent = false, -- Disable setting background
+		transparent = true, -- Disable setting background
 		terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
 		dim_inactive = true, -- Non focused panes set to alternative background
 		styles = { -- Style to be applied to different syntax groups
 			comments = "italic", -- Value is any valid attr-list value `:help attr-list`
 			conditionals = "NONE",
 			constants = "NONE",
-			functions = "italic,bold",
-			keywords = "bold",
+			functions = "bold",
+			keywords = "NONE",
 			numbers = "NONE",
 			operators = "NONE",
 			strings = "NONE",
-			types = "NONE",
+			types = "italic",
 			variables = "NONE",
 		},
 		inverse = { -- Inverse highlight for different types
