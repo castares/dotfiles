89929a50e3f642843618c3f3b90e4674ec10d75a
César C <cesar.castares@gmail.com>
2022-09-12 21:03:23 +0200

    [NVIM][Treesitter] Add text objects and motion keymaps.

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -37,4 +37,61 @@ configs.setup({
 		-- colors = {}, -- table of hex strings
 		-- termcolors = {} -- table of colour name strings
 	},
+	-- From "nvim-treesitter/nvim-treesitter-textobjects"
+	textobjects = {
+		select = {
+			enable = true,
+
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
+
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["if"] = { query = "@function.inner", desc = "inner function" },
+				["af"] = { query = "@function.outer", desc = "outer function" },
+				["ic"] = { query = "@class.inner", desc = "inner class" },
+				["ac"] = { query = "@class.outer", desc = "outer class" },
+			},
+			-- You can choose the select mode (default is charwise 'v')
+			selection_modes = {
+				["@parameter.outer"] = "v", -- charwise
+				["@function.outer"] = "V", -- linewise
+				["@class.outer"] = "<c-v>", -- blockwise
+			},
+			-- If you set this to `true` (default is `false`) then any textobject is
+			-- extended to include preceding xor succeeding whitespace. Succeeding
+			-- whitespace has priority in order to act similarly to eg the built-in
+			-- `ap`.
+			include_surrounding_whitespace = false,
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>>"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader><"] = "@parameter.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["gj"] = "@function.outer",
+				["gJ"] = "@class.outer",
+			},
+			goto_next_end = {
+				["[j"] = "@function.outer",
+				["[J"] = "@class.outer",
+			},
+			goto_previous_start = {
+				["gk"] = "@function.outer",
+				["gK"] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[k"] = "@function.outer",
+				["[K"] = "@class.outer",
+			},
+		},
+	},
 })
