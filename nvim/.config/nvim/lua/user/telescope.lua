7c9027c8302774e49c6fccb65731fc2edb7e8b66
César Castañón <cesar.castanon@piperlab.es>
2022-11-04 17:26:36 +0100

    [NVIM] Telescope: do not overwrite find_cmd

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -122,7 +122,7 @@ telescope.setup({
 			-- filetypes whitelist
 			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
 			filetypes = { "png", "webp", "jpg", "jpeg" },
-			find_cmd = "rg", -- find command (defaults to `fd`)
+			-- find_cmd = "rg", -- find command (defaults to `fd`)
 		},
 		file_browser = {
 			--theme = "ivy",
