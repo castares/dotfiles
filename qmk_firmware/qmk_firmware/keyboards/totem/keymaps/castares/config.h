25f491d0b5ed8848fdf8b683d2c4c6fcf5a80ae2
castares <cesar.castares@gmail.com>
2026-02-08 20:35:45 +0100

    [TOTEM] minor fixes

--- qmk_firmware/qmk_firmware/keyboards/totem/keymaps/castares/config.h
+++ qmk_firmware/qmk_firmware/keyboards/totem/keymaps/castares/config.h
@@ -1,8 +1,4 @@
 #pragma once
 
-#define MASTER_LEFT
-
 #define TAPPING_TERM 165
-
-// #define IGNORE_MOD_TAP_INTERRUPT //No longer necessary as it is now the default behaviour of mod-tap keys.
 #define QUICK_TAP_TERM 120
--- qmk_firmware/qmk_firmware/keyboards/totem/keymaps/castares/rules.mk
+++ qmk_firmware/qmk_firmware/keyboards/totem/keymaps/castares/rules.mk
@@ -3,3 +3,5 @@ CAPS_WORD_ENABLE = yes
 LTO_ENABLE = yes # Link Time Optimization
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes # Audio control and System control
+SPLIT_KEYBOARD = yes
+SPLIT_USB_DETECT = yes
